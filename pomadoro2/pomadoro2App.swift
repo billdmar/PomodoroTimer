1256a4402fc0894ff3593fb831fc5a8b00f6ca73
billdmar <billdmar@gmail.com>
2025-07-30 12:15:31 -0500

    Initial commit: Pomodoro timer app scaffold

---
+++ pomadoro2/pomadoro2App.swift
@@ -0,0 +1,17 @@
+//
+//  pomadoro2App.swift
+//  pomadoro2
+//
+//  Created by Bill Mar on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct pomadoro2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pomadoro2Tests/pomadoro2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  pomadoro2Tests.swift
+//  pomadoro2Tests
+//
+//  Created by Bill Mar on 7/30/25.
+//
+
+import Testing
+
+struct pomadoro2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
